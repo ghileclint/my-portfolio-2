86af0fcfa024f3845a8737fc08b48cbb94fc22cf
ghileclint <michaelclinton815@gmail.com>
2026-07-06 19:17:26 +0100

    added the scroll to the top function

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const menuBar = document.querySelector(".hamburger");
 const menuList = document.querySelector(".navBar nav");
 const menuItems = document.querySelectorAll(".navBar nav ul li a");
 
+const topBtn = document.querySelector("#scroll-top");
+
 let menuOpen = false;
 
 menuBar.addEventListener("click", () => {
@@ -22,3 +24,17 @@ menuItems.forEach((item) => {
     }
   });
 });
+
+window.addEventListener("scroll", () => {
+  if(window.scrollY > 300) {
+    topBtn.style.display = "block"
+  } else {
+    topBtn.style.display = "none"
+  }
+})
+
+topBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+  })
+})
